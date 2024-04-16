a6a9e815866cee3b256abfd17e29fcd2439b267f
BlueWeabo <ilia.iliev2005@gmail.com>
2024-04-16 11:16:25 +0300

    Add a helper enum

--- src/main/java/com/blueweabo/mutecore/MuTECore.java
+++ src/main/java/com/blueweabo/mutecore/MuTECore.java
@@ -3,6 +3,7 @@ package com.blueweabo.mutecore;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -10,11 +11,14 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = MuTECore.MODID, version = Tags.VERSION, name = "MuTECore", acceptedMinecraftVersions = "[1.7.10]")
+@Mod(modid = MuTECore.MODID, version = Tags.VERSION, name = "MuTECore", acceptedMinecraftVersions = "[1.7.10]", dependencies = MuTECore.DEPENDENCIES)
 public class MuTECore {
 
     public static final String MODID = "mutecore";
     public static final Logger LOG = LogManager.getLogger(MODID);
+    public static final String DEPENDENCIES = "required-after:gtnhlib@[0.2.7,);" +
+        "required-after:modularui;" +
+        "after:appliedenegistics2";
 
     @SidedProxy(clientSide = "com.blueweabo.mutecore.ClientProxy", serverSide = "com.blueweabo.mutecore.CommonProxy")
     public static CommonProxy proxy;
---
+++ src/main/java/com/blueweabo/mutecore/enums/LoadedMods.java
@@ -0,0 +1,7 @@
+package com.blueweabo.mutecore.enums;
+
+import cpw.mods.fml.common.Loader;
+
+public class LoadedMods {
+    public static final boolean AE2 = Loader.isModLoaded("appliedenegistics2");
+}
