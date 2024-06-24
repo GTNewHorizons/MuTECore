c0dd0fe1771678287bf58b49c26f580a797072e7
BlueWeabo <ilia.iliev2005@gmail.com>
2024-06-24 21:55:32 +0300

    Add a few more components

---
+++ src/main/java/com/blueweabo/mutecore/api/data/TickData.java
@@ -0,0 +1,14 @@
+package com.blueweabo.mutecore.api.data;
+
+public class TickData {
+
+    private int tick;
+
+    public int getTick() {
+        return tick;
+    }
+
+    public void setTick(int tick) {
+        this.tick = tick;
+    }
+}
